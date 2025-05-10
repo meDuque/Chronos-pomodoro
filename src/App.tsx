dd29b1e5ef278a297c153a754fcd9c1c0f6b65e7
¡Duque <atanaelleonardo@gmail.com>
2025-05-10 17:59:52 -0300

    chore: remove unused imports and components, updated App component to render Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { AboutPomodoro } from './pages/AboutPomodoro'
 import { Home } from './pages/Home'
-import { NotFound } from './pages/NotFound'
 import './styles/global.css'
 import './styles/theme.css'
 
 export function App() {
   return (
     <>
-      {/* <Home /> */}
-      <NotFound />
-      {/* <AboutPomodoro /> */}
+      <Home />
     </>
   )
 }
