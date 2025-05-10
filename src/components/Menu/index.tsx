a6933e50f4b43ffea76f48eaefb9fbac341cf1bd
¡Duque <atanaelleonardo@gmail.com>
2025-05-09 23:14:21 -0300

    feat: Update index.html to change language to pt-BR and add data-theme attribute. Update Menu component to add aria-label and title attributes to links. Update theme.css to define light theme styles.

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,16 +4,36 @@ import styles from './styles.module.css'
 export const Menu = () => {
   return (
     <nav className={styles.menu}>
-      <a href='#' className={styles.menuLink}>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='Ir para a Home'
+        title='Ir para a Home'
+      >
         <HouseIcon />
       </a>
-      <a href='#' className={styles.menuLink}>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='Ir para o Histórico'
+        title='Ir para o Histórico'
+      >
         <HistoryIcon />
       </a>
-      <a href='#' className={styles.menuLink}>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='Ir para as Configurações'
+        title='Ir para as Configurações'
+      >
         <SettingsIcon />
       </a>
-      <a href='#' className={styles.menuLink}>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='Mudar o tema da Aplicação'
+        title='Mudar o tema da Aplicação'
+      >
         <SunIcon />
       </a>
     </nav>
