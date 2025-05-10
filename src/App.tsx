e4850e1e0102d71471378b14d4f160a6aa2d759c
¡Duque <atanaelleonardo@gmail.com>
2025-05-09 22:53:35 -0300

    Removed unused import of StopCircleIcon from lucide-react and Footer from components/Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react'
+import { PlayCircleIcon } from 'lucide-react'
 import { Container } from './components/Container'
 import { CountDown } from './components/CountDown'
 import { Cycles } from './components/Cycles'
 import { DefaultButton } from './components/DefaultButton'
 import { DefaultInput } from './components/DefaultInput'
+import { Footer } from './components/Footer'
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
 import './styles/global.css'
 import './styles/theme.css'
-import { Footer } from './components/Footer'
 
 export function App() {
   return (
