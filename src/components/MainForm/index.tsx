2afc65a308975a1d7b3186e445a91f27e0e98cd5
¡Duque <atanaelleonardo@gmail.com>
2025-05-14 18:34:07 -0300

    feat: updated MainForm and TaskContextProvider with new functionality

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,5 +1,7 @@
 import { PlayCircleIcon } from 'lucide-react'
 import { type ReactNode, useRef, useState } from 'react'
+import { useTaskContext } from '../../contexts/TaskContext/UseTaskContext'
+import type { TaskModel } from '../../models/TaskModel'
 import { Cycles } from '../Cycles'
 import { DefaultButton } from '../DefaultButton'
 import { DefaultInput } from '../DefaultInput'
@@ -10,12 +12,44 @@ interface MainFormProps {
 }
 
 export function MainForm(_props: MainFormProps) {
-  // const [taskName, setTaskName] = useState('')
+  const { setState } = useTaskContext()
   const taskNameInput = useRef<HTMLInputElement>(null)
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
-    console.log('Deu certo!', Date.now())
+
+    if (taskNameInput.current === null) return
+
+    const taskName = taskNameInput.current.value.trim()
+
+    if (!taskName) {
+      alert('Task name is required')
+      return
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptedDate: null,
+      duration: 1,
+      type: 'workTime', // TODO: add type select
+    }
+
+    const secondsRemaining = newTask.duration * 60
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1, // TODO: add cycle select
+        secondsRemaining, // TODO: add seconds remaining select
+        formattedSecondsRemaining: '00:00', // TODO: add formatted seconds remaining select
+        tasks: [...prevState.tasks, newTask],
+      }
+    })
   }
 
   return (
