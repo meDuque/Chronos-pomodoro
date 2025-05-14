e4864b9b530062f9678fd38c20765f7cfadd58a7
¡Duque <atanaelleonardo@gmail.com>
2025-05-14 19:04:04 -0300

    feat: added getNextCycleType function and used it in MainForm component

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,6 +3,7 @@ import { type ReactNode, useRef, useState } from 'react'
 import { useTaskContext } from '../../contexts/TaskContext/UseTaskContext'
 import type { TaskModel } from '../../models/TaskModel'
 import { getNextCycle } from '../../utils/getNextCycle'
+import { getNextCycleType } from '../../utils/getNextCycleType'
 import { Cycles } from '../Cycles'
 import { DefaultButton } from '../DefaultButton'
 import { DefaultInput } from '../DefaultInput'
@@ -17,6 +18,7 @@ export function MainForm(_props: MainFormProps) {
   const taskNameInput = useRef<HTMLInputElement>(null)
 
   const nextCycle = getNextCycle(state.currentCycle)
+  const nextCycleType = getNextCycleType(nextCycle)
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
@@ -37,7 +39,7 @@ export function MainForm(_props: MainFormProps) {
       completeDate: null,
       interruptedDate: null,
       duration: 1,
-      type: 'workTime', // TODO: add type select
+      type: nextCycleType,
     }
 
     const secondsRemaining = newTask.duration * 60
