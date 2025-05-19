0e25c73dc51017ebbccc96a6abea05d85bc4be9c
¡Duque <atanaelleonardo@gmail.com>
2025-05-19 16:04:39 -0300

    feat: add TimerWorkerManager import and usage in MainForm component

---
+++ src/workers/TimerWorkerManager.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+let instance: TimerWorkerManager | null = null
+
+export class TimerWorkerManager {
+  private worker: Worker
+
+  private constructor() {
+    this.worker = new Worker(new URL('./timerWorker.js', import.meta.url))
+  }
+
+  static getInstance(): TimerWorkerManager {
+    if (!instance) {
+      instance = new TimerWorkerManager()
+    }
+
+    return instance
+  }
+
+  postMessage(message: any) {
+    this.worker.postMessage(message)
+  }
+
+  onmessage(callback: (e: MessageEvent) => void) {
+    this.worker.onmessage = callback
+  }
+
+  terminate() {
+    this.worker.terminate()
+    instance = null
+  }
+}
