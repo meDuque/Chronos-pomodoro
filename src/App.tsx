e81f6473a795b5249bf911f2f012ca67b26e26bd
¡Duque <atanaelleonardo@gmail.com>
2025-05-20 16:53:41 -0300

    feat: added toast notification component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
+import { ToastContainer } from 'react-toastify/unstyled'
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider'
 import { Home } from './pages/Home'
 
 import './styles/global.css'
 import './styles/theme.css'
+import { Bounce } from 'react-toastify'
 
 export function App() {
   return (
     <>
       <TaskContextProvider>
         <Home />
+
+        <ToastContainer
+          position='top-right'
+          autoClose={10000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme='light'
+          transition={Bounce}
+        />
       </TaskContextProvider>
     </>
   )
