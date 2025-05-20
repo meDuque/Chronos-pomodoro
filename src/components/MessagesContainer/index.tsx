8c467329b64cb79d665e5b4cf6acedcef655be6f
¡Duque <atanaelleonardo@gmail.com>
2025-05-20 17:29:05 -0300

    feat: add closeButton prop to MessagesContainer component

--- src/components/MessagesContainer/index.tsx
+++ src/components/MessagesContainer/index.tsx
@@ -21,6 +21,7 @@ export function MessagesContainer({ children }: MessagesContainerProps) {
         pauseOnHover
         theme='light'
         transition={Bounce}
+        closeButton={false}
       />
     </>
   );
