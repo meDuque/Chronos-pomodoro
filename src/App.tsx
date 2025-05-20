5200cfa4ec1b3c0108ffcdd418e3e232ccc40bd9
¡Duque <atanaelleonardo@gmail.com>
2025-05-20 19:45:47 -0300

    refactor routing and add scrollbar styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router'
 import { MessagesContainer } from './components/MessagesContainer'
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider'
-import { Home } from './pages/Home'
+import { MainRouter } from './routes/MainRouter'
 import './styles/global.css'
 import './styles/theme.css'
-import { AboutPomodoro } from './pages/AboutPomodoro'
-import { NotFound } from './pages/NotFound'
 
 export function App() {
   return (
     <>
       <TaskContextProvider>
         <MessagesContainer>
-          <BrowserRouter>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
-
-              <Route path='*' element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
+          <MainRouter />
         </MessagesContainer>
       </TaskContextProvider>
     </>
---
+++ src/routes/MainRouter/index.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router'
+import { AboutPomodoro } from '../../pages/AboutPomodoro'
+import { Home } from '../../pages/Home'
+import { NotFound } from '../../pages/NotFound'
+
+function ScrollOnTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
+
+export function MainRouter() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
+
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <ScrollOnTop />
+      </BrowserRouter>
+    </>
+  )
+}
